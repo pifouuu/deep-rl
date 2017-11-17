b04d8e189d1c6e733a16192b5df4936cac27e172
pifouuu <pierre00fournier@gmail.com>
2017-11-17 10:03:24 +0100

    scripts for cluster

--- ddpg/launcher.sh
+++ ddpg/launcher.sh
@@ -1,9 +1,39 @@
 #!/bin/bash
-#PBS -N ddpg_mountain_car_standard
-#PBS -o ddpg_mountain_car_standard.out
-#PBS -b ddpg_mountain_car_standard.err
-#PBS -m abe
-#PBS -M pierre.fournier@isir.upmc.fr
-#PBS -l walltime=01:00:00
-#PBS -l ncpus=8
-/usr/bin/python3.4 /home/fournier/deep-rl/ddpg/ddpg.py --number-runs 3 --summary-dir /home/fournier/deep-rl/ddpg/results/ --max-episodes 20 --episode-reset
+
+# Example submission of PBS jobs in a loop
+
+NUMBERS=$(seq 1 20) # Create an array of seed values from 1 to NSEEDS
+DDPG=/home/fournier/deep-rl/ddpg/ddpg.py
+LOGDIR=/home/fournier/deep-rl/ddpg/results/
+
+
+# Loop through the different seed values and submit a run for each
+
+for NUM in ${NUMBERS}
+do
+	# set the job name
+	NAME=goal_${NUM}
+	echo "Submitting: ${NAME}"
+
+	# Build a string called PBS which contains the instructions for your run
+	# This requests 1 node for 1 hour. Runs a program called "my_program" with an argument.
+
+	PBS="#!/bin/bash\n\
+	#PBS -N ${NAME}\n\
+	#PBS -l nodes=1:ppn=1\n\
+	#PBS -l walltime=2:00:00\n\
+	#PBS -o output/${NAME}.out\n\
+	#PBS -e error/${NAME}.err\n\
+	cd \$PBS_O_WORKDIR\n\
+	/usr/bin/python3.4 $DDPG --summary-dir $LOGDIR --max-episodes 500 --episode-reset --with-goal"
+
+	# Note that $PBS_O_WORKDIR is escaped ("\"). We don't want bash to evaluate this variable right now. Instead it will be evaluated when the command runs on the node.
+
+	# Echo the string PBS to the function qsub, which submits it as a cluster job for you
+	# A small delay is included to avoid overloading the submission process
+
+	echo -e ${PBS} | qsub
+	sleep 0.5
+	echo "done."
+
+done
--- ddpg/launcher_test_local.sh
+++ ddpg/launcher_test_local.sh
@@ -3,5 +3,5 @@ MAIN=/home/pierre/PycharmProjects/deep-rl/ddpg/ddpg.py
 LOGDIR=/home/pierre/PycharmProjects/deep-rl/ddpg/results/
 for i in {1..20}
 do
-    /usr/bin/python3.5 $MAIN --summary-dir $LOGDIR --delta 1 --max-episodes 500 --episode-reset
+    /usr/bin/python3.5 $MAIN --summary-dir $LOGDIR --max-episodes 500 --episode-reset
 done
