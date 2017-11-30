d9c0c52554c9ab3f27ca8833dc65f5be5ffcde22
osigaud <Olivier.Sigaud@upmc.fr>
2017-11-30 10:32:03 +0100

    added cluster_perf

---
+++ ddpg/cluster_perf.py
@@ -0,0 +1,12 @@
+#from ofp_perf_study import perf_study
+from standard_perf_study import perf_study
+import os
+
+# Configuration
+delta_clip = float(os.environ["DELTA"])
+trial = os.environ["TRIAL"]
+
+# Experiment attributes
+force = (os.environ.get("FORCE", "false") == "true")
+
+perf_study(delta_clip,trial)
