fb083501d14a5fb2fdf3ec955d1cd45a62d7d6fa
pifouuu <pierre00fournier@gmail.com>
2017-11-30 18:14:18 +0100

    debugging olivier's scripts

--- ddpg/perf_study.sh
+++ ddpg/perf_study.sh
@@ -11,6 +11,6 @@ do
     export LOGS
     export PERF_STUDY="perf_$SAMPLER"
     rm -f ${PERF_STUDY}.e*
-    qsub -N ${PERF_STUDY} -o "$LOGS/${PERF_STUDY}.out" -b "$LOGS/${PERF_STUDY}.err" -d $HOME/deep-rl/ddpg perf-submit.sh
+    qsub -N ${PERF_STUDY} -o "$LOGS/${PERF_STUDY}.out" -b "$LOGS/${PERF_STUDY}.err" -d $HOME/deep-rl/ddpg perf_submit.sh
   )
 done
