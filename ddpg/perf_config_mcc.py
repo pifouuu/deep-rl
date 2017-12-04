9382067acfdb140339da208dad14a8563da1a483
osigaud <Olivier.Sigaud@upmc.fr>
2017-12-04 10:11:24 +0100

    prepared an ofp run with us_frequent buffer (containing 50.000 samples and 1200 rewards) to run for 11 hours

--- ddpg/perf_config_mcc.py
+++ ddpg/perf_config_mcc.py
@@ -5,9 +5,9 @@ class PerfConfig(CommonConfig):
         # Basic variables
         super(PerfConfig, self).__init__(**kwargs)
 
-        #self.memory_file = "data/replay_buffer_us_frequent.p"
+        self.memory_file = "data/replay_buffer_us_frequent.p"
         #self.memory_file = "data/replay_buffer_us_fair_big.p"
-        self.memory_file = "data/replay_buffer_gep.p"
+        #self.memory_file = "data/replay_buffer_gep.p"
         self.reward_scaling = 1.0
         self.tau = 1e-3
         self.eval_episodes = 10 #number of episodes to run during evaluation', default=20
@@ -16,4 +16,4 @@ class PerfConfig(CommonConfig):
         self.eval_freq = 1000 #evaluation frequency', default=1000
         self.run_type = "ofp" #"standard"
         self.noise_factor = 0.3 #3.0
-        self.save_step_stats = True
+        self.save_step_stats = False
