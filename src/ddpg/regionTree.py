1c34aedea16e309824df320f8c0b1332a9ca70da
pifouuu <pierre00fournier@gmail.com>
2018-02-07 16:31:33 +0100

    coherent results of the region tree

--- src/ddpg/regionTree.py
+++ src/ddpg/regionTree.py
@@ -40,6 +40,8 @@ class Region(Box):
         high_left[dim] = split_val
         left = Region(self.low, high_left, maxlen=self.maxlen, n_cp = self.n_cp)
         right = Region(low_right, self.high, maxlen=self.maxlen, n_cp = self.n_cp)
+        left.CP = self.CP
+        right.CP = self.CP
         return left, right
 
     def append(self, point):
@@ -47,18 +49,15 @@ class Region(Box):
         self.update_CP()
 
     def update_CP(self):
-        CP = 0
-        if self.size < 2:
-            pass
-        else:
-            window_length = min([self.n_cp, self.size // 2])
-            rev = reversed(self.points)
-            for p in list(rev)[:window_length]:
+        if self.size > 2*self.n_cp:
+            CP = 0
+            rev = list(reversed(self.points))
+            for p in rev[:self.n_cp]:
                 CP += p.val
-            for p in list(rev)[window_length:2 * window_length]:
+            for p in rev[self.n_cp:2 * self.n_cp]:
                 CP -= p.val
-            CP /= 2 * window_length
-        self.CP = CP
+            self.CP = 1/2+CP / (2 * self.n_cp)
+        assert self.CP >= 0
 
     @property
     def size(self):
@@ -92,6 +91,11 @@ class RegionTree():
         self.active_regions = 0
         self.total_points = 0
         self.discarded = 0
+        self.ax = None
+        self.figure = None
+        self.lines = []
+        self.patches = []
+        self.points = []
 
     def init_tree(self, region):
         self.region_array[1] = region
@@ -145,7 +149,9 @@ class RegionTree():
             region = self.region_array[idx]
             region_left = self.region_array[2*idx]
             region_right = self.region_array[2*idx + 1]
-            if self.split_eval_1(region_left, region_right) < self.split_min:
+            split_eval = self.split_eval_1(region_left, region_right)
+            to_merge = self.is_leaf(2*idx) and self.is_leaf(2*idx+1) and split_eval < self.split_min
+            if to_merge:
                 self.region_array[2 * idx + 1] = Region()
                 self.region_array[2 * idx] = Region()
                 print('delete split')
@@ -174,8 +180,9 @@ class RegionTree():
         best_val = None
         split = False
         region = self.region_array[idx]
+        eval_splits = []
         for dim in range(region.shape[0]):
-            for split_val in np.linspace(region.low[dim], region.high[dim], self.n_split+2)[1:-1]:
+            for num_split, split_val in enumerate(np.linspace(region.low[dim], region.high[dim], self.n_split+2)[1:-1]):
                 temp_left, temp_right = region.split(dim, split_val)
                 for point in region.points:
                     if temp_left.contains(point.pos):
@@ -187,9 +194,12 @@ class RegionTree():
                         raise(RuntimeError)
                 temp_left.update_CP()
                 temp_right.update_CP()
-                split_eval = self.split_eval_1(temp_left, temp_right)
-                if split_eval > self.split_min and split_eval > best_split_eval:
-                    best_split_eval = split_eval
+                split_eval_1 = self.split_eval_1(temp_left, temp_right)
+                split_eval_2 = self.split_eval_2(temp_left, temp_right)
+                eval_splits.append((dim, split_val, split_eval_1, split_eval_2))
+
+                if split_eval_1 > self.split_min and split_eval_1 > best_split_eval:
+                    best_split_eval = split_eval_1
                     best_split_right = temp_right
                     best_split_left = temp_left
                     best_dim = dim
@@ -212,8 +222,9 @@ class RegionTree():
 
             region.dim_split = best_dim
             region.val_split = best_val
-        else:
-            print('no split')
+            print('splint succeeded: dim=', best_dim, ' val=', best_val)
+        # else:
+            # print('no split')
 
     def points_in_leaves(self):
         return self._point_in_leaves(1)
@@ -228,51 +239,53 @@ class RegionTree():
         return res
 
     def displayTree(self, dims):
-        self.fig = plt.figure()
+        self.figure = plt.figure()
         init_region = self.region_array[1]
         self.ax = plt.axes()
         self.ax.set_xlim(init_region.low[dims[0]], init_region.high[dims[0]])
         self.ax.set_ylim(init_region.low[dims[1]], init_region.high[dims[1]])
 
-        point_list = self._display_tree(1, dims)
-        # x,y = zip(*[(point[0],point[1]) for point in point_list])
-        # print(len(x))
-        # ax.scatter(x,y, s=0.2, c='red')
-        # plt.show()
+    def compute_image(self, dims, with_points=False):
+        self.lines.clear()
+        self.patches.clear()
+        self.points.clear()
+        self._compute_image(1, dims, with_points)
+        print('max_cp: ', self.max_CP)
+        print('min_cp', self.min_CP)
 
-    def _display_tree(self, idx, dims):
+    def _compute_image(self, idx, dims, with_points=False):
         region = self.region_array[idx]
-        point_list = []
-        point_list_left = []
-        point_list_right = []
         if region.dim_split is None:
             angle = (region.low[dims[0]], region.low[dims[1]])
             width = region.high[dims[0]] - region.low[dims[0]]
             height = region.high[dims[1]] - region.low[dims[1]]
-            self.ax.add_patch(
-                patches.Rectangle(angle,
+            # print('region: ', region.low, ': ', region.CP)
+            if self.max_CP == 0:
+                color = 0
+            else:
+                color = region.CP/self.max_CP
+            self.patches.append(patches.Rectangle(angle,
                                   width,
                                   height,
                                   fill=True,
-                                  facecolor=Blues(region.CP/self.max_CP),
+                                  facecolor=Blues(color),
                                   edgecolor=None,
-                                  alpha=1))
-            for points in region.points:
-                point_list.append(points.pos)
+                                  alpha=0.8))
+            if with_points:
+                for point in region.points:
+                    self.points.append(point)
         else:
             if region.dim_split == dims[0]:
-                line1_xs = 2*[region.val_split]
+                line1_xs = 2 * [region.val_split]
                 line1_ys = [region.low[dims[1]], region.high[dims[1]]]
-                self.ax.add_line(lines.Line2D(line1_xs, line1_ys, linewidth=2, color='blue'))
+                self.lines.append(lines.Line2D(line1_xs, line1_ys, linewidth=2, color='blue'))
             elif region.dim_split == dims[1]:
-                line1_ys = 2*[region.val_split]
+                line1_ys = 2 * [region.val_split]
                 line1_xs = [region.low[dims[0]], region.high[dims[0]]]
-                self.ax.add_line(lines.Line2D(line1_xs, line1_ys, linewidth=2, color='blue'))
+                self.lines.append(lines.Line2D(line1_xs, line1_ys, linewidth=2, color='blue'))
 
-            point_list_left += self._display_tree(2*idx, dims)
-            point_list_right += self._display_tree(2*idx + 1, dims)
-
-        return point_list+point_list_left+point_list_right
+            self._compute_image(2 * idx, dims)
+            self._compute_image(2 * idx + 1, dims)
 
     def find_sum_idx(self, sum):
         """Find the highest index `i` in the array such that
@@ -301,9 +314,9 @@ class RegionTree():
         sample = region.sample()
         return sample
 
-    def sample(self):
+    def sample(self, p_rnd):
         p = np.random.random()
-        if p<0.3:
+        if p<p_rnd:
             return self.sample_random()
         else:
             return self.sample_prop()
@@ -321,13 +334,10 @@ class RegionTree():
     def sum_CP(self):
         return self.region_array[1].sum_CP
 
-def sigmoid(x, l):
-    return 1 / (1 + math.exp(-l*x))
-
 class demo():
     def __init__(self):
-        self.tree = RegionTree(10, 10, 10)
-        init_region = Region(np.array([-1.2, -0.07]), np.array([0.6, 0.07]), maxlen = 1000, n_cp = 10)
+        self.tree = RegionTree(n_regions=6, n_split=10, split_min=1e-6)
+        init_region = Region(np.array([-1.2, -0.07]), np.array([0.6, 0.07]), maxlen = 200, n_cp = 50)
         self.tree.init_tree(init_region)
 
         zone1 = Region(np.array([-1.2, -0.07]), np.array([-0.6, 0]))
@@ -339,73 +349,60 @@ class demo():
         self.zones = [zone1, zone2, zone3, zone4, zone5, zone6]
         self.samples_per_zone = 6*[0]
         self.comp_per_zone = 6*[0]
-        self.zone_difficulties = [1, 2, 4, 7, 11, 16]
+        self.zone_difficulties = [0, 200, 400, 800, 1000, 1200]
 
         self.iteration = 0
+        np.random.seed(None)
 
-    def iter(self,iteration):
-        goal = self.tree.sample()
-
+    def compute_comp(self, goal):
         goal_zone = None
         i = 0
         while goal_zone is None:
             if self.zones[i].contains(goal):
                 goal_zone = self.zones[i]
             i += 1
-        n_samples = self.samples_per_zone[i-1]
-        # comp = sigmoid(n_samples-self.zone_difficulties[i-1]*10, l=2)
-        if i<4:
-            if iteration<4000:
-                comp = n_samples
-            else:
-                comp = 0
+        n_samples = self.samples_per_zone[i - 1]
+
+        if n_samples < self.zone_difficulties[i - 1]:
+            comp = 0
         else:
-            if iteration<4000:
-                comp = 0
-            else:
-                comp = n_samples
-        self.comp_per_zone[i-1] += comp
-        # comp = np.min([1, (n_samples / 500) ** (self.zone_difficulties[i - 1] / 6)])
-        # print('goal zone: ', i-1, ', comp: ', comp)
+            comp = np.min([1, (n_samples - self.zone_difficulties[i - 1]) / 1000])
 
         self.samples_per_zone[i - 1] += 1
+        # print("zone ", i-1, 'samples: ', self.samples_per_zone[i-1])
+        return comp
+
+# Kolmogorov smirnof
+    def iter(self):
+        goal = self.tree.sample(0.1)
+        comp = self.compute_comp(goal)
         point = Point(goal, comp)
         self.tree.insert_point(point)
         self.iteration += 1
 
-    def run(self):
-        figs = []
-        for i in range(10000):
-            self.iter(i)
-            if i % 100 == 0 and i > 0:
-                print('iter ', i)
-                for zone, (nb, comp) in enumerate(zip(self.samples_per_zone,self.comp_per_zone)):
-                    print('zone ', zone, ', nb: ', nb, ', comp: ', comp/nb)
-                print('\n')
-                # self.samples_per_zone = 6*[0]
-                # self.comp_per_zone = 6*[0]
-                self.tree.displayTree(dims=[0, 1])
-                self.tree.fig.canvas.draw()
-                figs.append(np.array(self.tree.fig.canvas.renderer._renderer))
-                plt.close(self.tree.fig)
-        print('discarded: ', self.tree.discarded)
-        return figs
+    def updatefig(self, i, with_points=False):
+        self.tree.ax.lines.clear()
+        self.tree.ax.patches.clear()
+        for _ in range(100):
+            self.iter()
+        print(self.iteration)
+        self.tree.compute_image(dims=[0,1], with_points=with_points)
+        for line in self.tree.lines:
+            self.tree.ax.add_line(line)
+        for patch in self.tree.patches:
+            self.tree.ax.add_patch(patch)
+        if with_points:
+            x,y,z = zip(*[(point.pos[0], point.pos[1], point.val) for point in self.tree.points])
+            sizes = [0.01 + ze for ze in z]
+            self.tree.ax.scatter(x,y, s=sizes, c='red')
+        return self.tree.ax,
 
+    def run(self):
+        self.tree.displayTree(dims=[0,1])
+        ani = animation.FuncAnimation(self.tree.figure, self.updatefig, frames=100, interval=200, blit=True)
+        plt.show()
 
 if __name__ == "__main__":
     demo = demo()
-    figs = demo.run()
-
-    fig = plt.figure()
-    im = plt.imshow(figs[0], animated=True)
-
-
-    def updatefig(i):
-        im.set_array(figs[i + 1])
-        return im,
-
-
-    ani = animation.FuncAnimation(fig, updatefig, frames=98, interval=50)
-    plt.show()
-
+    demo.run()
 
